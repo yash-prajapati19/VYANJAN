ac271d87c606ecde4ac64183563829e3582f25e2
Renji <aadarshjha1401@gmail.com>
2023-02-01 23:03:14 +0530

    corrected package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,11 @@
-import { Fragment } from "react";
-import Header from "./components/layout/Header/Header";
-import Footer from "./components/layout/Footer/Footer";
-import {Routes , Route } from 'react-router-dom'
-import Cart from './components/Cart/Cart'
-import Home from './components/Home/Home'
-import Shipping from './components/Shipping/Shipping'
-import Order from './components/Order/Order'
+import React from 'react';
 
 
 function App() {
   return (
-  <Fragment>
-    <Header />
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/cart' element={<Cart />} />
-        <Route exact path='/shipping' element={<Shipping />} />
-        <Route exact path='/order' element={<Order />} />
-      </Routes>
-    <Footer />
-  </Fragment>
+  <>
+    
+  </>
   );
 }
 
