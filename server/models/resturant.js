662176f2b38870836874e209902d91dbd772eab6
Yash Prajapati <yp54139@gmail.com>
2023-02-03 23:16:24 +0530

    done

--- server/models/resturant.js
+++ server/models/resturant.js
@@ -32,7 +32,7 @@ const resturantSchema = new Schema(
       type: String,
       required: true,
     },
-    admin_id: { type: Schema.Types.ObjectId, required: true, ref: "AdminData" },
+    admin_id: { type: Schema.Types.ObjectId, required: true, default:"", ref: "AdminData" },
     items: [{ type: Schema.Types.ObjectId, ref: "Item" }],
   },
   { timestamps: true }
