84759a983f75c4c041f2b852de04ee107d7f7d7c
Renji <aadarshjha1401@gmail.com>
2023-02-04 13:13:22 +0530

    merged rohit's

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,11 @@ import {Routes, Route} from 'react-router-dom';
 import Login from './components/Admin/Login';
 import Register from './components/Admin/Register';
 import OTP from './components/Admin/OTP'
-import CurrentOrderDetails from './components/User/CurrentOrderDetails'
+import CurrentOrderDetails from './components/User/CurrentOrderDetails';
+import Bites from './components/User/Bites/Bites'
+import RestaurantSuggestion from "./components/User/Suggestion/RestaurantSuggestion";
+import OrderHistory from "./components/User/OrderHistory/OrderHistory";
+import AdminOrderHistory from "./components/Admin/AdminOrderHistory/AdminOrderHistory";
 
 
 function App() { 
@@ -15,6 +19,10 @@ function App() {
   return (
   <Fragment>
     <Routes>
+      <Route exact path="/bites" element={<Bites />} />
+      <Route exact path="/suggestion" element={<RestaurantSuggestion />} />
+      <Route exact path="/orderhistory" element={<OrderHistory />} />
+      <Route exact path="/admin/orderhistory" element={<AdminOrderHistory />} />
       <Route exact path='/registerRestaurant' element={<Register />} />
       <Route exact path='/adminlogin' element={<Login setUrlMail={setOtpVerificationMail} setTempLogin={setTempLogin} />} />
       <Route exact path='/otp/*' element={<OTP urlMail={otpVerificationMail} tempLogin={tempLogin} setTempLogin={setTempLogin} />} />
---
+++ client/src/components/User/Suggestion/RestaurantSuggestion.js
@@ -0,0 +1,49 @@
+import React, { Fragment } from 'react'
+import "./RestaurantSuggestion.css";
+import SuggestionCard from "./SuggestionCard";
+import Restaurant1 from "../../../images/Restaurant1.jpeg";
+import Restaurant2 from "../../../images/Restaurant2.jpg";
+
+const RestaurantSuggestion = () => {
+  const restaurant = [
+    {
+       name:"Restaurant 1 ",
+       image:Restaurant1,
+       address:"Address 1",
+       price:"₹399"
+    },
+    {
+       name:"Restaurant 2", 
+       image:Restaurant2,
+       address:"Address 2",
+       price:"₹599"
+    },
+  ]
+   
+  return (
+    <Fragment>
+      <div className="suggestion">
+        <div className="suggestionHeading">Order From Your Favorite Eatery</div>
+        <div className="container" id="container">
+          {restaurant.map((data,i) => (
+            <SuggestionCard 
+            key={i}
+            name={data.name}
+            image={data.image}
+            address={data.address}
+            price={data.price}
+            />
+          ))}
+          {/* <SuggestionCard
+            name="Restaurant 1"
+            image={Restaurant1}
+            address="Address 1"
+            price="₹399"
+          /> */}
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default RestaurantSuggestion
